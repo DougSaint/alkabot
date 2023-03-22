3991682c1397a81417622bc83eed73d8277e9e40
Douglas Santos <ds4208151@gmail.com>
2023-03-22 16:45:02 -0300

    gh-pages.

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Navbar } from "./components";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import BlogProvider from "./context/BlogProvider.jsx";
-import { Home, Users, Posts, Post, UserDetails } from './pages';
+import { Home, Users, Posts, Post, UserDetails, NotFound } from './pages';
 
 
 function App() {
@@ -12,11 +12,12 @@ function App() {
         <div className="dark:bg-gray-800 min-h-screen">
           <Navbar />
           <Routes>
-            <Route path="/" element = {<Home />} />
+            <Route exact path ="/" element = {<Home />} />
             <Route path="/posts/:page" element = {<Posts />} />
             <Route path="/post/:id" element = {<Post />} />
             <Route path="/users" element = {<Users />} />
             <Route path="/user/:id" element = {<UserDetails />} />
+            <Route path="*" element = {<NotFound />} />
           </Routes>
         </div>
       </BrowserRouter>
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function NotFound() {
+  return (
+    <div>
+      <h1 className="text-center text-3xl text-slate-700 dark:text-slate-200">
+        Pagina não encontrada
+      </h1>
+    </div>
+  );
+}
